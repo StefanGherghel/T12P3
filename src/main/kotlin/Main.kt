f3f69fe64891726c87e751db00ee6f2dcdb47d3c
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-24 23:23:29 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 import kotlin.properties.Delegates
 
 fun main(args: Array<String>) {
-    println("de cate ori sa apara fiecare numar in lista?: ")
-    val n = readln().toInt()
-    val listaMea = listOf(1,2,3,4)
-    val listaMeaNoua = listaMea.flatMap { var l = mutableListOf(it); for ( i in 1..n-1) l.add(it); l}
-    println(listaMeaNoua)
+    val strRau = "aaaabbccccccc"
+    val strBun = strRau.asSequence().distinct().joinToString()
+    println(strBun)
 }
