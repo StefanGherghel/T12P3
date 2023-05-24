e27ff3f89a12657d132e0b1f94bbf4e7ca012120
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-24 22:50:43 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,6 @@
-import java.time.LocalDate
-import java.util.Date
-import java.util.Formatter
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-
-fun String.strToDate(format: DateTimeFormatter): LocalDate
-{
-    return LocalDate.parse(this, format)
-}
-
 
 fun main(args: Array<String>) {
-    var formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-    val date: String = "12-09-1980"
-    println(date.strToDate(formatter).toString())
+    var mapaMea= mapOf(1 to "abc", 2 to "def", 3 to "ghi");
+    var mapaMeaNoua = mapaMea.map { (k, v) -> v to k }
+    print(mapaMeaNoua)
 }
