a4f57805c87a2e8c5f64edac702d0c02f3ce6ac9
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-24 22:59:32 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
+import kotlin.properties.Delegates
 
+fun isPrimm(x: Int): Boolean
+{
+    var div = 2
+    while(div < x/2)
+        if(x % (div++)==0)
+            return false
+    return true
+}
 fun main(args: Array<String>) {
-    var mapaMea= mapOf(1 to "abc", 2 to "def", 3 to "ghi");
-    var mapaMeaNoua = mapaMea.map { (k, v) -> v to k }
-    print(mapaMeaNoua)
+    var Prim: Int by Delegates.vetoable(5) { property, oldValue, newValue -> isPrimm(newValue) }
+
+    println(Prim)
+
+    Prim = 6
+    println(Prim)
+
+    Prim = 7
+    println(Prim)
+
 }
