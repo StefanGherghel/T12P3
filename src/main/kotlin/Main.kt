57ab072ad47411d42006ced7c9c9411c41394e52
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-25 14:56:11 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,23 @@
+import java.io.File
 import kotlin.properties.Delegates
+fun Char.CaesarReplace(offset: Int): Char
+{
 
-fun main(args: Array<String>) {
-    var listaMea = listOf(1, 21, 75, 39, 7, 2, 35, 3, 31, 7, 8)
-
-    listaMea = listaMea.filter { it > 5 }
-    println( listaMea )                                                                 // a)
-
-    var listaMea2 = listaMea.zipWithNext().filterIndexed { index, pair -> index%2 == 0  }
-    println( listaMea2 )                                                                //b)
-
-    var listaMea3 = listaMea2.map{it.first*it.second}
-    println( listaMea3 )                                                                //c)
-
-    var listaMea4 = listaMea3.sum()                                                 //d)
-    listaMea3.forEach{print("+ $it ")}
-    println("= "+listaMea4)
+    if(! this.isWhitespace()) return this+offset
+    else return this
 }
+
+fun readFileAsLinesUsingReadLines(fileName: String): List<String> = File(fileName).readLines()
+fun main(args: Array<String>)
+{
+    val textCitit = readFileAsLinesUsingReadLines("data.txt")
+    println(textCitit)
+
+    var textChar = mutableListOf<Char>()
+    textCitit.map{it.toCharArray()}.forEach{it.forEach { textChar.add(it.CaesarReplace(offset = 2) )}}
+    val mesajCodat = textChar.toString().filter {it.isLetter()}
+
+    println(mesajCodat)
+}
+//[acesta este un fisier care urmeaza sa fie codat cu cifrul Caesar cu offset dat]
+// -> ceguvcguvgwphkukgtectgwtogccuchkgeqfcvewekhtwnEcguctewqhhugvfcv
