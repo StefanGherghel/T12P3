a5ee536dec3059e566f6eb137dbbb02c89969ea0
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-25 13:44:51 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import kotlin.properties.Delegates
 
 fun main(args: Array<String>) {
-    val strRau = "aaaabbccccccc"
-    val strBun = strRau.asSequence().distinct().joinToString()
-    println(strBun)
+    val strRau = "aaaabbcccccccddd"
+    val strAparitii = strRau.asSequence().groupingBy{ it }.eachCount().filter{ it.value >= 1}.toString().filter { it.isDigit() }
+    val strFinal = strRau.groupBy { it }.values.toList().zip(strAparitii.toList()).toString().filter { it.isLetterOrDigit() }
+    println("StrFinal: $strFinal")
+    //println("strAparitii: $strAparitii")
 }
