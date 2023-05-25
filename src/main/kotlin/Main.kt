1c832e54de6cff662b5913a6597fe7bbc34f3a56
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-25 16:43:28 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,20 @@
-import java.io.File
-import kotlin.properties.Delegates
-fun Char.CaesarReplace(offset: Int): Char
-{
+import java.lang.Math.abs
+import java.lang.Math.pow
+import kotlin.math.sqrt
 
-    if(! this.isWhitespace()) return this+offset
-    else return this
-}
-
-fun readFileAsLinesUsingReadLines(fileName: String): List<String> = File(fileName).readLines()
 fun main(args: Array<String>)
 {
-    val textCitit = readFileAsLinesUsingReadLines("data.txt")
-    println(textCitit)
-
-    var textChar = mutableListOf<Char>()
-    textCitit.map{it.toCharArray()}.forEach{it.forEach { textChar.add(it.CaesarReplace(offset = 2) )}}
-    val mesajCodat = textChar.toString().filter {it.isLetter()}
-
-    println(mesajCodat)
+    print("Nr puncte: ")
+    val nrPuncte: Int = readln().toInt()
+    var puncte = List(nrPuncte){
+        print("coordonate punct: ")
+        readln().toInt() to readln().toInt()
+    }
+    println(puncte)
+    var temp = puncte.zipWithNext()
+    var perim: Double = 0.0
+    perim += sqrt(pow(abs(temp.first().first.first.toDouble() - temp.last().second.first.toDouble()),2.0)+pow(abs(temp.first().first.second.toDouble() - temp.last().second.second.toDouble()),2.0))
+    temp.forEach{perim += sqrt(pow(abs(it.first.first.toDouble() - it.second.first.toDouble()),2.0)+pow(abs(it.first.second.toDouble() - it.second.second.toDouble()),2.0)) }
+    println(temp)
+    println("Perimetrul este: $perim")
 }
-//[acesta este un fisier care urmeaza sa fie codat cu cifrul Caesar cu offset dat]
-// -> ceguvcguvgwphkukgtectgwtogccuchkgeqfcvewekhtwnEcguctewqhhugvfcv
